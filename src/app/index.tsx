de7e195ea4af875f4ee1df31f912e001f3786e64
yuka sakamaki <yuka.sakamaki@ebisol.co.jp>
2024-04-29 20:06:09 +0900

    update/mark up memo list

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,47 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 const Index = (): JSX.Element => {
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      <View>
+        <View>
+          <Text>Memo app</Text>
+          <Text>ログアウト</Text>
+        </View>
+      </View>
+      <View>
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2024年1月1日 10:00</Text>
+          </View>
+          <View>
+            <Text>x</Text>
+          </View>
+        </View>
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2024年1月1日 10:00</Text>
+          </View>
+          <View>
+            <Text>x</Text>
+          </View>
+        </View>
+      </View>
+      <View>
+        <Text>+</Text>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
 
 export default Index
